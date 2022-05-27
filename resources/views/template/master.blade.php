6002bb805b8e645e496fc3bcb47006c8dfa0d3ad
dzaki236 <dzakiahnafz@gmail.com>
2022-05-27 14:15:24 +0700

    update pewea

--- resources/views/template/master.blade.php
+++ resources/views/template/master.blade.php
@@ -7,6 +7,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     <meta name="google" content="notranslate">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- PWA  -->
+    <meta name="theme-color" content="#6777ef" />
+    <link rel="apple-touch-icon"
+        href="https://static.vecteezy.com/system/resources/previews/000/549/469/original/j-letter-vector-logo-template-illustration-design.jpg">
+    <link rel="manifest" href="{{ asset('/manifest.json') }}">
     <title>Tokoh [Jhon Dalton / JD]</title>
     <!-- Font Awesome -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
@@ -17,12 +22,15 @@
     @yield('css')
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
-        *{
+
+        * {
             font-family: 'Poppins', sans-serif;
         }
-        body{
+
+        body {
             background-color: rgb(250, 246, 241);
         }
+
     </style>
 </head>
 
@@ -30,11 +38,24 @@
     <div class="container p-3">
         @yield('content')
     </div>
-    <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js' integrity='sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==' crossorigin='anonymous'></script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js'
+        integrity='sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ=='
+        crossorigin='anonymous'></script>
     <script>
-        $(document).bind("contextmenu",function(e){ return false; });
+        $(document).bind("contextmenu", function(e) {
+            return false;
+        });
+    </script>
+    <script src="{{ asset('/sw.js') }}"></script>
+    <script>
+        if (!navigator.serviceWorker.controller) {
+            navigator.serviceWorker.register("/sw.js").then(function(reg) {
+                console.log("Service worker has been registered for scope: " + reg.scope);
+            });
+        }
     </script>
     @yield('js')
+
 </body>
 
 </html>
